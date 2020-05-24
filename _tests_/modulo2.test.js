ca7a84e27b98292a4e532a19cef9846f1497a805
PedroSpiet <pedro.silva940@etec.sp.gov.br>
2020-05-23 23:32:17 -0300

    Finalizando teste entre módulos

---
+++ _tests_/modulo2.test.js
@@ -0,0 +1,5 @@
+const modulo2 = require('../src/modulo2/modulo2');
+
+test('5 * 5 deve dar 25', () => {
+    expect(modulo2.func2(5,5)).toBe(25)
+});
---
+++ src/modulo2/modulo2.js
@@ -0,0 +1,5 @@
+const modulo2 = {
+    func2: (a, b) => a * b
+}
+
+module.exports = modulo2;
